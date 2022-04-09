f4e93b379b1f353afebcb85e5c75a8e87eea1d5e
Clebson Moura <clebson.sh@gmail.com>
2022-04-09 18:46:29 -0300

    mais testes para queryString

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,6 +9,27 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Clebson&profession=developer')
   })
+
+  it('should create a valid query string even when a array is passed as value', () => {
+    const obj = {
+      name: 'Clebson',
+      abilities: ['JS', 'TDD'],
+    }
+
+    expect(queryString(obj)).toBe('name=Clebson&abilities=JS,TDD')
+  })
+
+  it('should throw a error when a object is passed', () => {
+    const obj = {
+      name: 'Clebson',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    }
+
+    expect(() => queryString(obj)).toThrowError()
+  })
 })
 
 // describe("Query string to object", () => {
