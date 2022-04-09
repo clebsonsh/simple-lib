7e2a1f925d05575cc290bc19fde29fe8ef19a18e
Clebson Moura <clebson.sh@gmail.com>
2022-04-09 17:37:42 -0300

    converter objeto para querystring com testes

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,7 +1,6 @@
 module.exports.sum = (num1, num2) => {
   const int1 = parseInt(num1, 10);
   const int2 = parseInt(num2, 10);
-  console.log(int1, int2);
 
   if (Number.isNaN(int1) || Number.isNaN(int2)) {
     throw new Error("Please check you input");
---
+++ src/lib/queryString.spec.js
@@ -0,0 +1,15 @@
+const { queryString } = require("./queryString");
+
+describe("Object to query string", () => {
+  it("should create a valid query string when an object is provided", () => {
+    const obj = {
+      name: "Clebson",
+      profession: "developer",
+    };
+
+    expect(queryString(obj)).toBe("name=Clebson&profession=developer");
+  });
+});
+
+// describe("Query string to object", () => {
+// });
